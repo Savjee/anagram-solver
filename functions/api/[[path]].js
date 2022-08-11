380b517850a1cbee9cb37b9a6bd09e6e5aab3b9c
Xavier Decuyper <hi@savjee.be>
2022-08-11 11:31:39 +0200

    Fixing variables naming...

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -13,9 +13,9 @@ export async function onRequestGet({ request, env }) {
     // If we land here, the object exists in R2. Now make sure that it's cached
     // aggressively by the browser as the file won't change in the future.
     const responseHeaders = new Headers();
-    obj.writeHttpMetadata(headers);
-    headers.set('etag', obj.httpEtag);
-    headers.append('Cache-Control', 'public, max-age=604800, immutable');
+    obj.writeHttpMetadata(responseHeaders);
+    responseHeaders.set('etag', obj.httpEtag);
+    responseHeaders.append('Cache-Control', 'public, max-age=604800, immutable');
 
     return new Response(obj.body, {
       headers: responseHeaders
