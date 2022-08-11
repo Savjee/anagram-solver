63d7824824fd8bbe9c85ef6f31547e8b05a986fb
Xavier Decuyper <hi@savjee.be>
2022-08-11 11:19:01 +0200

    Trying out Cloudflare Pages Functions + R2

---
+++ functions/api/[[path]].js
@@ -0,0 +1,21 @@
+export async function onRequestGet({ request, env }) {
+  const requestUrl = new URL(request.url);
+
+  // Try to fetch the object from R2
+  const obj = await env.R2_API_STORAGE.get(requestUrl.pathname);
+
+  if (obj === null) {
+    return new Response('Not found', { status: 404 });
+  }
+
+  // If we land here, the object exists in R2. Now make sure that it's cached
+  // aggressively by the browser as the file won't change in the future.
+  const responseHeaders = new Headers();
+  obj.writeHttpMetadata(headers);
+  headers.set('etag', obj.httpEtag);
+  headers.append('Cache-Control', 'public, max-age=604800, immutable');
+
+  return new Response(obj.body, {
+    headers: responseHeaders
+  });
+}
