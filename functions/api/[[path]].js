6bf40a4705b4d5285a605dccda6cacaacdaf84cf
Xavier Decuyper <hi@savjee.be>
2022-08-11 11:28:17 +0200

    Function: catch all errors

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -1,22 +1,26 @@
 export async function onRequestGet({ request, env }) {
-  const requestUrl = new URL(request.url);
-  const objKey = requestUrl.pathname.slice(1);
+  try{
+    const requestUrl = new URL(request.url);
+    const objKey = requestUrl.pathname.slice(1);
 
-  // Try to fetch the object from R2
-  const obj = await env.R2_API_STORAGE.get(objKey);
+    // Try to fetch the object from R2
+    const obj = await env.R2_API_STORAGE.get(objKey);
 
-  if (obj === null) {
-    return new Response('Not found', { status: 404 });
-  }
+    if (obj === null) {
+      return new Response('Not found', { status: 404 });
+    }
 
-  // If we land here, the object exists in R2. Now make sure that it's cached
-  // aggressively by the browser as the file won't change in the future.
-  const responseHeaders = new Headers();
-  obj.writeHttpMetadata(headers);
-  headers.set('etag', obj.httpEtag);
-  headers.append('Cache-Control', 'public, max-age=604800, immutable');
+    // If we land here, the object exists in R2. Now make sure that it's cached
+    // aggressively by the browser as the file won't change in the future.
+    const responseHeaders = new Headers();
+    obj.writeHttpMetadata(headers);
+    headers.set('etag', obj.httpEtag);
+    headers.append('Cache-Control', 'public, max-age=604800, immutable');
 
-  return new Response(obj.body, {
-    headers: responseHeaders
-  });
+    return new Response(obj.body, {
+      headers: responseHeaders
+    });
+  }catch(e){
+    return new Response("Error occured: " + JSON.stringify(e), {status: 500});
+  } 
 }
